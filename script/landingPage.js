295d2bd8628b43f46d3641aab4f2601bfd577728
@JuliusDev <raagasjulius34@gmail.com>
2023-03-01 23:19:46 +0800

    Game completed!

--- script/landingPage.js
+++ script/landingPage.js
@@ -1,13 +1,19 @@
+// Use strict mode to catch errors earlier
 "use strict";
+
+// Find the container and buttons in the HTML document
 const container = document.querySelector(".main");
 const difficultyButtons = document.querySelectorAll(".dfBtn");
-
 const playBtn = document.querySelector("#playBtn");
+
+// Disable the play button by default
 playBtn.disabled = true;
 
+// Initialize variables
 let secondClicked = false;
-
 let gameLevel = null;
+
+// Functions to set and get the game level
 const setGameLevel = (lvl) => {
   gameLevel = lvl;
 };
@@ -16,6 +22,7 @@ const getGameLevel = () => {
   return gameLevel;
 };
 
+// When the container is clicked, disable the play button and enable difficulty buttons
 container.addEventListener("click", (e) => {
   if (e.target === container) {
     playBtn.disabled = true;
@@ -25,17 +32,18 @@ container.addEventListener("click", (e) => {
   }
 });
 
+// When a difficulty button is clicked, set the game level and disable other difficulty buttons
 difficultyButtons.forEach((outerButton) => {
   outerButton.addEventListener("click", (e) => {
     setGameLevel(outerButton.value);
-    let buttons = difficultyButtons;
-    buttons.forEach((innerButton) => {
+    difficultyButtons.forEach((innerButton) => {
       innerButton.disabled = innerButton.value !== outerButton.value;
     });
     playBtn.disabled = false;
   });
 });
 
+// When the play button is clicked, store the game level in local storage and navigate to the playground page
 playBtn.addEventListener("click", () => {
   localStorage.setItem("gameLevel", getGameLevel());
   window.location.href = "playground.html";
--- script/onPlaying.js
+++ script/onPlaying.js
@@ -1,18 +1,136 @@
 "use strict";
-const instruction = document.querySelector("#instructionContainer");
-let gameLevel = localStorage.getItem("gameLevel");
 
-let levelTimer = {
-  easy: 15,
-  medium: 10,
-  hard: 4,
-  expert: 2,
+// Get the DOM elements
+const timerDisplay = document.querySelector("#instructionContainer");
+const statusContainer = document.querySelector("#correctAnswer");
+const playerAnswer = document.querySelector("#playerAnswer");
+const startAndSubmit = document.querySelector("#startSubmit");
+
+// Game settings
+let levelSettings = [
+  { level: "easy", mins: 8 * 60, range: 10 },
+  { level: "medium", mins: 5 * 60, range: 30 },
+  { level: "hard", mins: 3 * 60, range: 100 },
+  { level: "expert", mins: 1 * 60, range: 1000 },
+];
+let totalPoints = 0;
+let gameLevel = null;
+let gameSetting = null;
+
+// setting the defaults
+statusContainer.innerHTML = `<h2>?</h2>`;
+startAndSubmit.value = "start";
+startAndSubmit.innerHTML = "Start";
+playerAnswer.disabled = true;
+
+// set game level
+const setGameLevel = (lvl) => {
+  gameLevel = lvl;
 };
 
-for (let [key, value] of Object.entries(levelTimer)) {
-  if (gameLevel === key) {
-    instruction.innerHTML = `<h2>Guess as many numbers as possible within ${value} minutes!</h2>`;
-  }
-}
+// get game level
+const getGameLevel = () => {
+  return gameLevel;
+};
 
-// TODO continue for web functionality
+// set the game level from locaStorage
+setGameLevel(localStorage.getItem("gameLevel"));
+gameSetting = levelSettings.find((setting) => setting.level === getGameLevel());
+timerDisplay.innerHTML = `<h2>Guess as many numbers as possible within ${
+  gameSetting.mins / 60
+} minutes!</h2>`;
+
+// Start timer
+const startTimer = (duration, display) => {
+  if (duration < 0) {
+    resetGame(display);
+    return;
+  }
+
+  // format time 00:00
+  display.innerHTML = formatTime(duration);
+
+  setTimeout(() => {
+    startTimer(duration - 1, display);
+  }, 1000);
+};
+
+// Format time
+const formatTime = (seconds) => {
+  let minutes = Math.floor(seconds / 60);
+  seconds = seconds % 60;
+  return `${minutes.toString().padStart(2, "0")}:${seconds
+    .toString()
+    .padStart(2, "0")}`;
+};
+
+// Generate a Random number
+const generateRandNum = (range) => {
+  let randomNumber = Math.floor(Math.random() * range) + 1;
+  return randomNumber;
+};
+
+// Checking the player answer
+const checkAnswer = (answer, playerAnswer) => {
+  const input = document.querySelector("#playerAnswer");
+  let feedback = "";
+
+  if (answer === playerAnswer) {
+    feedback = "correct";
+    correctAnswer = generateRandNum(gameSetting.range);
+    totalPoints++;
+  } else if (playerAnswer < answer) {
+    feedback = "higher";
+    totalPoints = Math.max(0, totalPoints - 1);
+  } else {
+    feedback = "lower";
+    totalPoints = Math.max(0, totalPoints - 1);
+  }
+
+  statusContainer.innerHTML = `<h2>${feedback.toUpperCase()} ${
+    feedback === "correct" ? "🎉" : feedback === "higher" ? "👆" : "👇"
+  }</h2>`;
+
+  // reset
+  input.value = "";
+  setTimeout(() => {
+    statusContainer.innerHTML = `<h2>?</h2>`;
+  }, 1000);
+};
+
+const resetGame = (display) => {
+  display.innerHTML = "Times Up!";
+  setTimeout(() => {
+    display.innerHTML = `<h2>Your total points</h2>`;
+    statusContainer.innerHTML = `<h2>${totalPoints}🎉</h2>`;
+  }, 2000);
+  setTimeout(() => {
+    statusContainer.innerHTML = `<h2>?</h2>`;
+    display.innerHTML = `<h2>Guess as many numbers as possible within ${
+      gameSetting.mins / 60
+    } minutes!</h2>`;
+  }, 4000);
+  startAndSubmit.value = "start";
+  startAndSubmit.innerHTML = "Start";
+  playerAnswer.disabled = true;
+};
+
+// generate the correct answer from generate random number function
+let correctAnswer = generateRandNum(gameSetting.range);
+startAndSubmit.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (e.target.value === "start") {
+    startAndSubmit.value = "submit";
+    startAndSubmit.innerHTML = "submit";
+    playerAnswer.disabled = false;
+    startTimer(gameSetting.mins, timerDisplay);
+  } else {
+    let inputAnswer = parseInt(playerAnswer.value);
+    if (inputAnswer) {
+      checkAnswer(correctAnswer, inputAnswer);
+    } else {
+      console.log(correctAnswer);
+      alert("Enter a number to make a guess!");
+    }
+  }
+});
